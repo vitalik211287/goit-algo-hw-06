85bffae3580de77a95cd564c57a9bc84c8e3c08c
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2025-06-21 23:45:11 +0300

    edd second tasck

--- one.py
+++ one.py
@@ -1,11 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from collections import deque
 
-# G = nx.Graph()
-# G.add_nodes_from(["B", "C", "D"])
-# G.add_edges_from([("A", "C"), ("B", "C"), ("B", "D")])
-# print(G.nodes())  # ['A', 'B', 'C', 'D']
-# print(G.edges())  # [('A', 'B'), ('A', 'C'), ('B', 'C'), ('B', 'D')]
 
 # Завдання 1: Створення графа транспортної мережі
 G = nx.Graph()
@@ -40,3 +36,49 @@ print("Кількість ребер:", G.number_of_edges())
 print("Ступені вершин:")
 for node in G.nodes():
     print(f"{node}: {G.degree(node)}")
+
+    # Завдання 2
+
+def dfs_recursive(graph, vertex, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(vertex)
+    print(vertex, end=' ')  # Відвідуємо вершину
+    for neighbor in graph[vertex]:
+        if neighbor not in visited:
+            dfs_recursive(graph, neighbor, visited)
+
+
+def bfs_recursive(graph, queue, visited=None):
+    # Перевіряємо, чи існує множина відвіданих вершин, якщо ні, то ініціалізуємо нову
+    if visited is None:
+        visited = set()
+    # Якщо черга порожня, завершуємо рекурсію
+    if not queue:
+        return
+    # Вилучаємо вершину з початку черги
+    vertex = queue.popleft()
+    # Перевіряємо, чи відвідували раніше дану вершину
+    if vertex not in visited:
+        # Якщо не відвідували, друкуємо вершину
+        print(vertex, end=" ")
+        # Додаємо вершину до множини відвіданих вершин.
+        visited.add(vertex)
+        # Додаємо невідвіданих сусідів даної вершини в кінець черги.
+        queue.extend(set(graph[vertex]) - visited)
+    # Рекурсивний виклик функції з тією ж чергою та множиною відвіданих вершин
+    bfs_recursive(graph, queue, visited)
+
+# print("DFS:", dfs_recursive(G, "A"))
+# print("BFS:", bfs_recursive(G, deque(["A"])))
+
+# dfs_recursive(G, "A", visited=None)
+# bfs_recursive(G, deque(["A"]), visited=None)
+
+print("\nDFS:", end=" ")
+dfs_recursive(G, "A")
+
+# print("\nBFS (ітеративно):", bfs(G, "A"))
+
+print("\nBFS:", end=" ")
+bfs_recursive(G, deque(["A"]))
